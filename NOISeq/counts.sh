72b98b57ad0d1bf2b3162cfd13b7bdeba806f5e7
golden75 <neranjan007@gmail.com>
2019-12-13 21:01:56 -0500

    add transcript length

--- NOISeq/counts.sh
+++ NOISeq/counts.sh
@@ -4,4 +4,4 @@ awk '{print $1 "\t" $4}' ../Counts/K23/abundance.tsv > K23.counts
 awk '{print $1 "\t" $4}' ../Counts/K32/abundance.tsv > K32.counts
 awk '{print $1 "\t" $4}' ../Counts/U32/abundance.tsv > U32.counts
 awk '{print $1 "\t" $4}' ../Counts/U13/abundance.tsv > U13.counts
-
+awk '{print $1 "\t" $2}' ../Counts/K23/abundance.tsv > length.txt
